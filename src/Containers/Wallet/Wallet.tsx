4f0c0824c2905adb5a29ed1b769d715f8a3f81ad
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-18 19:10:26 +0000

    loading contracts

--- src/Containers/Wallet/Wallet.tsx
+++ src/Containers/Wallet/Wallet.tsx
@@ -1,16 +1,90 @@
 import React, { Component } from 'react';
+import truffleContract from 'truffle-contract';
+import MIXRContract from '../../contracts/MIXR.json';
+import SampleERC20Contract from '../../contracts/SampleERC20.json';
+import getWeb3 from '../../utils/getWeb3';
+
 import './Wallet.css';
 
-
-class Wallet extends Component {
+interface IWalletType { name: string; priceUSD: number; value: number; }
+interface IMIXRContractState {
+    getApprovedTokens: () => Promise<[[IWalletType], number]>;
+}
+interface IWalletState {
+    mixrContract: IMIXRContractState;
+    sampleERC20: object;
+    userAccount: string;
+    web3: object;
+    walletInfo: [IWalletType];
+}
+class Wallet extends Component<{}, IWalletState> {
+    /**
+     * @ignore
+     */
     constructor(props: any) {
         super(props);
+
+        /* this.state = {
+            walletInfo: [{} as IWalletType],
+        }; */
     }
 
-    public componentDidMount() {
-        //
+    /**
+     * @ignore
+     */
+    public async componentDidMount() {
+        try {
+            // load web3 and the usar accoun
+            const web3 = await getWeb3();
+            const accounts = await web3.eth.getAccounts();
+
+            // tslint:disable-next-line: no-unused-expression
+            new Promise(async (resolve, reject) => {
+                // load MIXR contract
+                const ContractMIXR = truffleContract(MIXRContract);
+                ContractMIXR.setProvider(web3.currentProvider);
+                const instanceMIXR = await ContractMIXR.deployed();
+
+                // load sampleERC20 contract
+                const ContractSampleERC20 = truffleContract(SampleERC20Contract);
+                ContractSampleERC20.setProvider(web3.currentProvider);
+                const instanceSampleERC20 = await ContractSampleERC20.deployed();
+
+                /* const walletInfo = [
+                    {
+                        name: 'USDT',
+                        priceUSD: 21.012,
+                        value: 21.056,
+                    },
+                ]; */
+
+                // update component state
+                this.setState({
+                    mixrContract: instanceMIXR,
+                    sampleERC20: instanceSampleERC20,
+                });
+                /* this.setState({
+                    walletInfo,
+                }); */
+                resolve();
+            });
+
+            // update component state
+            this.setState({
+                userAccount: accounts[0],
+                web3,
+            });
+        } catch (error) {
+            // if an error happen, log it
+            // TODO: should we log somewhere else apart from the browser console?
+            console.log('Failed to load web3, accounts, or contract. Check console for details.');
+            console.log(error);
+        }
     }
 
+    /**
+     * @ignore
+     */
     public render() {
         return (
             <div className="Wallet">
@@ -32,26 +106,28 @@ class Wallet extends Component {
                                 <span className="Wallet__grid-title--underline">DEPOSIT</span>
                             </span>
                         </div>
-
-                        {/* TODO: Create a for each to loop out each cryptocurrency in wallet  */}
                         {this.renderWalletInfo()}
-
                     </div>
                 </div>
             </div>
         );
     }
 
-    private renderWalletInfo() {
+    /**
+     * Get wallet user balance and render all information
+     */
+    public renderWalletInfo() {
         {
-            const walletInfo = [
-                {
-                    name: 'USDT',
-                    priceUSD: 21.012,
-                    value: 21.056,
-                },
-            ];
+            if (this.state === undefined || this.state === null) {
+                return;
+            }
 
+            const { walletInfo } = this.state;
+            if (walletInfo === undefined /* [{} as IWalletType] */) {
+                return;
+            }
+
+            // render array data
             return walletInfo.map((element) => {
                 return (
                     <React.Fragment key={element.name}>

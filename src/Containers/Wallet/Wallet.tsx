9de2b229b12a1b679d51302a358a8cddc1e62896
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-19 18:09:19 +0000

    load wallet balances and improve comments

--- src/Containers/Wallet/Wallet.tsx
+++ src/Containers/Wallet/Wallet.tsx
@@ -1,23 +1,49 @@
 import BigNumber from 'bignumber.js';
 import React, { Component } from 'react';
 import truffleContract from 'truffle-contract';
+import IERC20Contract from '../../contracts/IERC20.json';
 import MIXRContract from '../../contracts/MIXR.json';
-import SampleERC20Contract from '../../contracts/SampleERC20.json';
 import getWeb3 from '../../utils/getWeb3';
 
 import './Wallet.css';
 
+/**
+ * Interface for wallet definition
+ */
 interface IWalletType { name: string; priceUSD: number; value: number; }
-interface IMIXRContractState {
-    getApprovedTokens: () => Promise<[[IWalletType], number]>;
+/**
+ * Interface for web3 definition
+ */
+interface IWeb3Type {
+    currentProvider?: object;
 }
+/**
+ * Interface for IERC20 definition
+ */
+interface IERC20Type {
+    (address: string): IERC20Type;
+    decimals: () => Promise<number>;
+    balanceOf: (user: string) => Promise<number>;
+}
+/**
+ * Interface for mixr contract definition
+ */
+interface IMIXRContractType extends IERC20Type {
+    getApprovedTokens: () => Promise<[[string], number]>;
+}
+/**
+ * Interface for wallet component state definition
+ */
 interface IWalletState {
-    mixrContract?: IMIXRContractState;
-    sampleERC20?: object;
+    mixrContract?: IMIXRContractType;
+    IERC20?: IERC20Type;
     userAccount?: string;
-    web3?: object;
+    web3?: IWeb3Type;
     walletInfo?: IWalletType[];
 }
+/**
+ * The wallet component
+ */
 class Wallet extends Component<{}, IWalletState> {
     /**
      * @ignore
@@ -25,7 +51,6 @@ class Wallet extends Component<{}, IWalletState> {
     constructor(props: any) {
         super(props);
     }
-
     /**
      * @ignore
      */
@@ -34,65 +59,14 @@ class Wallet extends Component<{}, IWalletState> {
             // load web3 and the usar accoun
             const web3 = await getWeb3();
             const accounts = await web3.eth.getAccounts();
-
             // update component state
             this.setState({
                 userAccount: accounts[0],
                 web3,
             });
-
-            // tslint:disable-next-line: no-unused-expression
-            new Promise(async (resolve, reject) => {
-                // load MIXR contract
-                const ContractMIXR = truffleContract(MIXRContract);
-                ContractMIXR.setProvider(web3.currentProvider);
-                const instanceMIXR = await ContractMIXR.deployed();
-
-                // load sampleERC20 contract
-                const ContractSampleERC20 = truffleContract(SampleERC20Contract);
-                ContractSampleERC20.setProvider(web3.currentProvider);
-                const instanceSampleERC20 = await ContractSampleERC20.deployed();
-
-                // load main wallet balances
-                const { userAccount } = this.state;
-                // get the token decimals
-                const mixrDecimals = new BigNumber(await instanceMIXR.decimals()).toNumber();
-                // get balance and devide by tokens decimals so we can see a friendly number
-                const mixrBalance = new BigNumber(
-                    await instanceMIXR.balanceOf(userAccount),
-                ).dividedBy(10 ** mixrDecimals);
-                // same as above!
-                const sampleERC20Decimals = new BigNumber(await instanceSampleERC20.decimals()).toNumber();
-                const sampleERC20Balance = new BigNumber(
-                    await instanceSampleERC20.balanceOf(userAccount),
-                ).dividedBy(10 ** sampleERC20Decimals);
-
-                // save in a state array to render
-                const walletInfo = [
-                    {
-                        name: 'USDT',
-                        priceUSD: 21.012,
-                        value: 21.056,
-                    },
-                    {
-                        name: 'MIX',
-                        priceUSD: mixrBalance.toNumber(),
-                        value: mixrBalance.toNumber(),
-                    },
-                    {
-                        name: 'SAMPLE',
-                        priceUSD: sampleERC20Balance.toNumber(),
-                        value: sampleERC20Balance.toNumber(),
-                    },
-                ];
-
-                // update component state
-                this.setState({
-                    mixrContract: instanceMIXR,
-                    sampleERC20: instanceSampleERC20,
-                    walletInfo,
-                });
-                resolve();
+            // load platform info async
+            this.loadContracts().then(() => {
+                this.loadUserBalances();
             });
         } catch (error) {
             // if an error happen, log it
@@ -101,7 +75,6 @@ class Wallet extends Component<{}, IWalletState> {
             console.log(error);
         }
     }
-
     /**
      * @ignore
      */
@@ -132,21 +105,19 @@ class Wallet extends Component<{}, IWalletState> {
             </div>
         );
     }
-
     /**
      * Get wallet user balance and render all information
      */
-    public renderWalletInfo() {
+    private renderWalletInfo() {
         {
             if (this.state === null || this.state === undefined) {
                 return;
             }
-
+            // get wallet info form state
             const { walletInfo } = this.state;
             if (walletInfo === null || walletInfo === undefined) {
                 return;
             }
-
             // render array data
             return walletInfo.map((element) => {
                 return (
@@ -166,6 +137,81 @@ class Wallet extends Component<{}, IWalletState> {
                 );
             });
         }
+    }
+    /**
+     * Load contracts async
+     */
+    private loadContracts() {
+        return new Promise(async (resolve, reject) => {
+            // web3 form state
+            const { web3 } = this.state;
+            if (web3 === undefined) {
+                return;
+            }
+            // load MIXR contract
+            const ContractMIXR = truffleContract(MIXRContract);
+            ContractMIXR.setProvider(web3.currentProvider);
+            const instanceMIXR = await ContractMIXR.deployed();
+            // load the ERC20 interface
+            const IERC20 = truffleContract(IERC20Contract);
+            // update component state
+            this.setState({
+                IERC20,
+                mixrContract: instanceMIXR,
+            });
+            resolve();
+        });
+    }
+    /**
+     * Load balance async
+     * This should happen after @loadContracts
+     */
+    private loadUserBalances() {
+        return new Promise(async (resolve, reject) => {
+            // load main wallet balances
+            const { userAccount, mixrContract, IERC20 } = this.state;
+            if (userAccount === undefined || mixrContract === undefined || IERC20 === undefined) {
+                return;
+            }
+            // get the token decimals
+            const mixrDecimals = new BigNumber(await mixrContract.decimals()).toNumber();
+            // get balance and devide by tokens decimals so we can see a friendly number
+            const mixrBalance = new BigNumber(
+                await mixrContract.balanceOf(userAccount),
+            ).dividedBy(10 ** mixrDecimals);
+            // same as above!
+            const approved: [[string], number] = await mixrContract.getApprovedTokens();
+            const approvedTokensAddress: [string] = approved[0];
+            const totalApprovedTokens: number = new BigNumber(approved[1]).toNumber();
+            // save in a state array to render
+            const walletInfo = [
+                {
+                    name: 'MIX',
+                    priceUSD: mixrBalance.toNumber(),
+                    value: mixrBalance.toNumber(),
+                },
+            ];
+            // iterate over accepted tokens to add them of state component for rendering
+            for (let i = 0; i < totalApprovedTokens; i += 1) {
+                // get token info
+                // TODO: we actualy need a method to get decimals!
+                const sampleDecimals = new BigNumber(18).toNumber();
+                const sampleBalance = new BigNumber(
+                    await IERC20(approvedTokensAddress[i]).balanceOf(userAccount),
+                ).dividedBy(10 ** sampleDecimals);
+                // add it to the array
+                walletInfo.push({
+                    name: 'SAMPLE',
+                    priceUSD: sampleBalance.toNumber(),
+                    value: sampleBalance.toNumber(),
+                });
+            }
+            // update component state
+            this.setState({
+                walletInfo,
+            });
+            resolve();
+        });
     }
 }
 

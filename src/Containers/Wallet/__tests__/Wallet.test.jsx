78fc4a46d752789821cf0ed0440646e20273b4c3
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-22 12:26:32 +0000

    wallet test improved

--- src/Containers/Wallet/__tests__/Wallet.test.jsx
+++ src/Containers/Wallet/__tests__/Wallet.test.jsx
@@ -1,6 +1,5 @@
-import { expect } from 'chai';
+/* globals describe test jest expect */
 import { mount, configure } from 'enzyme';
-import { waitForState } from 'enzyme-async-helpers';
 import Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 // eslint-disable-next-line no-unused-vars
@@ -11,14 +10,31 @@ import Wallet from '../Wallet.tsx';
 configure({ adapter: new Adapter() });
 // the following line will instruct Jest
 // to use the mock class instead of the real one
-// eslint-disable-next-line no-undef
 jest.mock('../../../utils/getWeb3');
-// eslint-disable-next-line no-undef
-test('Should mount Wallet with success', async () => {
-    // Render wallet and all information associated
-    const wrapper = mount(<Wallet />);
-    // wait until it's fully loaded
-    await waitForState(wrapper, state => state.ready === true);
-    // expects the state 'ready' to be true, which means, it's loaded
-    expect(wrapper.instance().state.ready).to.be.equal(true);
+
+describe('some test!', () => {
+    test('Should mount Wallet with success', async (done) => {
+        // define an expected value
+        const expectedWalletInfo = [
+            {
+                name: 'MIX',
+                priceUSD: 0,
+                value: 0,
+            },
+        ];
+        // Render wallet and all information associated
+        const wrapper = mount(<Wallet />);
+        // wait until component did load finishes
+        await wrapper.instance().componentDidMount();
+        // wait for one more second, might be rendering
+        setTimeout(() => {
+            // update the wrapper to get new changes
+            wrapper.update();
+            // compare the actual value with the expected
+            expect(JSON.stringify(wrapper.instance().state.walletInfo))
+                .toBe(JSON.stringify(expectedWalletInfo));
+            // tell the test that it's done
+            done();
+        }, 1000);
+    });
 });

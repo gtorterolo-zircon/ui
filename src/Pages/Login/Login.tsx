937b9d331fe774d0bb563d045720eb0c6a4b3b95
Connor Todd <connortodd0304@gmail.com>
2019-03-04 15:09:01 +0000

    Seperate the login overlay from the login background

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
-// import Logo from '../../Assets/cementDAO-logo.png';
-import Logo from '../../Assets/img/cement-logo.svg';
+import LoginOverlay from '../../Components/LoginOverlay/LoginOverlay';
+
 import loginImg1 from '../../Assets/img/homepage-slider-img-1.svg';
 import sliderNext from '../../Assets/img/slider-next.svg';
-
-import MetamaskLogo from '../../Assets/img/metamask-logo.svg';
 
 import './Login.css';
 
@@ -29,57 +27,17 @@ class Login extends Component {
     public render() {
         return (
             <div>
+                <LoginOverlay
+                    click={this.handleLogin}
+                />
 
-                <div className="Login__grid-left">
-                    <img className="Login__logo" src={Logo} alt="cementDAO logo" />
-
-                    <div className="Login__grid-left-outer-container">
-                        <div className="Login__grid-left-top-container">
-                            <p className="Login__grid-left-title">
-                                CONNECT YOUR WALLET
-                                <br />
-                                AND START EXCHANGING
-                                <br />
-                                STABLECOIN NOW
-                            </p>
-                            <button
-                                type="button"
-                                className="Login__grid-left-button"
-                                onClick={this.handleLogin}
-                            >
-                            <span>
-                                <img className="Login__grid-left-button-logo" src={MetamaskLogo} />
-                            </span>
-                                METAMASK
-                            </button>
-                            <div className="Login__grid-left-CTA-container">
-                                <p className="Login__grid-left-CTA">
-                                    Don
-                                    <span>&#39;</span>
-                                    t have a Metamask account?
-                                    <br />
-                                    <a href="https://metamask.io/" target="_blank">
-                                        <span className="Login__grid-left-CTA--underline">CREATE ONE NOW</span>
-                                    </a>
-                                </p>
-                            </div>
-                            <span className="Login__grid-left-CTA--small Login__grid-left-CTA--underline">
-                                Terms of Service
-                            </span>
-                            <p className="Login__grid-left-CTA--small">&copy; Copyright 2019 by 1A1Z Ltd.</p>
-                            <p className="Login__grid-left-CTA--small">All rights reserved.</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
+                {/* Login Background */}
                 <div className="Login__grid">
-                    <div className="Login__grid-right"></div>
-                    <div className="Login__grid-right">
-                        <img className="Login__grid-right-logo" src={loginImg1} alt="cementDAO logo" />
-                        <p className="Login__grid-right-title">CEMENT IS A BASKET OF MANY STABLECOINS</p>
-                        <p className="Login__grid-right-content">
+                    <div className="Login__grid-inner"></div>
+                    <div className="Login__grid-inner">
+                        <img className="Login__grid-logo" src={loginImg1} alt="cementDAO logo" />
+                        <p className="Login__grid-title">CEMENT IS A BASKET OF MANY STABLECOINS</p>
+                        <p className="Login__grid-content">
                             Stablecoin holders can deposit their stablecoins into the
                             Cement Mixer (that&#39;s what we&#39;re calling the basket), to
                             benefit from increased liquidity, greater stability and
